dbfb5b5cdd17bd034015938119f1f840275ff643
SagaMS <91miablond@gmail.com>
2025-01-06 17:00:19 +0700

    feat:start single list task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if(l===null){
+    return l;
+}
+while(l.value === k){
+    l = l.next;
+}
+
+let head = l;
+while(head.next !== null){
+    if(head.next.value ===k){
+        head.next = head.next.next
+    }
+    head = head.next;
+}
+return l;
 }
 
 module.exports = {
